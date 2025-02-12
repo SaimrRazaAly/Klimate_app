4d636aeacc22b6d10c3d6076a42bf1210c25dab2
SaimRazaAly <saimrazateach2010@gmail.com>
2025-02-11 20:55:26 -0800

    installed react qurey (tanstack)

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,25 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import { ThemeProvider } from "./context/Theme_Provider";
 import DashBoard from "./pages/DashBoard";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 const App = () => {
   return (
-    <BrowserRouter>
-      <ThemeProvider defaultTheme="dark">
-        <Layout>
-          <Routes>
-            <Route path="/" element={<DashBoard/>}/>
-            <Route path="/city/:cityName" element={<DashBoard/>}/>
-          </Routes>
-        </Layout>
-      </ThemeProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider defaultTheme="dark">
+          <Layout>
+            <Routes>
+              <Route path="/" element={<DashBoard />} />
+              <Route path="/city/:cityName" element={<DashBoard />} />
+            </Routes>
+          </Layout>
+        </ThemeProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
